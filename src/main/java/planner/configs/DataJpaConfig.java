ed639007c4543d8499fdccaca63dc97e76a212ba
yevheniiatsiurupa <tsiurupa_yev@mail.ru>
2020-02-22 16:18:50 +0300

    fixed imports

--- src/main/java/planner/configs/DataJpaConfig.java
+++ src/main/java/planner/configs/DataJpaConfig.java
@@ -1,8 +1,6 @@
 package planner.configs;
 
 import org.apache.commons.dbcp2.BasicDataSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
